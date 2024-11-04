c4e8dc46d10e332f1ec72ac8dace97fd7f48361a
Pawel Paluch <info@pixelvision.pl>
2024-11-05 00:02:27 +0100

    Refactor App component: replace CSS layout with styled-components Wrapper for better styling consistency

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,35 @@
-import './App.css';
-import './index.css';
+import React, { useState } from 'react';
 import { Form } from './Form';
 import { Clock } from './Clock';
-
+import { Wrapper } from './styled';
+import { currencies } from './currencies';
 
 function App() {
+    const [result, setResult] = useState(null); 
+
+    const calculateResult = (currency, amount) => {
+        const rate = currencies.find(({ short }) => short === currency)?.rate;
+
+        if (!rate) {
+            console.error("Nie znaleziono kursu dla wybranej waluty.");
+            return;
+        }
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount / rate,
+            currency,
+        });
+    };
+
     return (
-        <div className="app">
+        <Wrapper>
             <Clock />
-            <Form />
-        </div>
+            <Form
+                result={result}
+                calculateResult={calculateResult}
+            />
+        </Wrapper>
     );
 }
 
