c7b11edee23ee7f52edfaa331406a935b2a967d3
Pawel Paluch <info@pixelvision.pl>
2025-01-09 22:35:30 +0100

    Removed unused state and currencies array from Index.js

--- src/App/index.js
+++ src/App/index.js
@@ -2,33 +2,12 @@ import React, { useState } from 'react';
 import { Form } from './Form';
 import { Clock } from './Clock';
 import { Wrapper } from './styled';
-import { currencies } from './currencies';
 
 function App() {
-    const [result, setResult] = useState(null); 
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies.find(({ short }) => short === currency)?.rate;
-
-        if (!rate) {
-            console.error("Nie znaleziono kursu dla wybranej waluty.");
-            return;
-        }
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        });
-    };
-
     return (
         <Wrapper>
             <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
+            <Form />
         </Wrapper>
     );
 }
